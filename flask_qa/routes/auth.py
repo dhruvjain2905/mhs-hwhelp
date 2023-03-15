d98fe8fad9ea08a36906726fc892fe07ea34464f
Parul Jain <paruljain@paruls-air.myfiosgateway.com>
2023-03-14 21:17:38 -0400

    requirements

--- flask_qa/routes/auth.py
+++ flask_qa/routes/auth.py
@@ -20,9 +20,9 @@ def register():
             flash('Username is already in use. Please try something different', category='error')
         elif confirm_pass != unhashed_password:
             flash("Passwords are different! Please enter the same password.", category='error')
-        elif len(name) < 1:
+        elif len(name) < 4:
             flash('Username is too short. Must be at least 4 characters.', category='error')
-        elif len(unhashed_password) < 1:
+        elif len(unhashed_password) < 4:
             flash('Password is too short. Must be at least 4 characters', category='error')
         else:
             if name == "MHS":
